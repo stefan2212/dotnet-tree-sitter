2761f0b587e1031cf34f0931faaf2b55578a9f89
Stefan Cernescu <stefan.cernescu94@gmail.com>
2023-07-20 08:58:50 +0300

    Correct some issues with OsX build

--- buildOsx.py
+++ buildOsx.py
@@ -6,12 +6,12 @@ DLYB = "dylib"
 def build_main_lib():
     print(" -- building main libraries")
     native_lib_dir = "tree-sitter/lib/src"
-    dotnet_lib_dir = "/usr/local/lib"
+    system_lib_dir = "/usr/local/lib"
     run([
         "gcc",
         "-fPIC",
         "-shared",
-        "-o", f"{dotnet_lib_dir}/libtree-sitter.{DLYB}",
+        "-o", f"{system_lib_dir}/libtree-sitter.{DLYB}",
         f"{native_lib_dir}/lib.c",
         "free.c",
         f"-I{native_lib_dir}",
@@ -23,12 +23,13 @@ def build_lang(native_name, cs_name, *files):
     print(" -- building", native_name, "language support")
     print("    -- building native library")
     native_dir = f"langs-native/tree-sitter-{native_name}/src"
-    dotnet_dir = f"/usr/local/lib.{cs_name}"
+    dotnet_dir = f"TreeSitter.{cs_name}"
+    system_lib_dir = f"/usr/local/lib"
     run([
         "gcc",
         "-fPIC",
         "-shared",
-        "-o", f"{dotnet_dir}/libtree-sitter-{native_name}.{DLYB}",
+        "-o", f"{system_lib_dir}/libtree-sitter-{native_name}.{DLYB}",
         *[f"{native_dir}/{file}" for file in files],
         f"-I{native_name}"
     ], check=True)
