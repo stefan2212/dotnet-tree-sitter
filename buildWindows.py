13be747f96bb886ae229cdf011b67c2ec960faa6
Stefan Cernescu <stefan.cernescu94@gmail.com>
2023-07-26 10:14:29 +0300

    Add buildWindows script

---
+++ buildWindows.py
@@ -0,0 +1,52 @@
+from subprocess import run
+from node_generator import generate
+
+DLL = "dll"
+
+def build_main_lib():
+    print(" -- building main libraries")
+    native_lib_dir = "tree-sitter/lib/src"
+    dotnet_lib_dir = "TreeSitter"
+    run([
+        "gcc",
+        "-fPIC",
+        "-shared",
+        "-o", f"{dotnet_lib_dir}/tree-sitter.{DLL}",
+        f"{native_lib_dir}/lib.c",
+        "free.c",
+        f"-I{native_lib_dir}",
+        f"-I{native_lib_dir}/../include",
+    ], check=True)
+
+
+def build_lang(native_name, cs_name, *files):
+    print(" -- building", native_name, "language support")
+    print("    -- building native library")
+    native_dir = f"langs-native/tree-sitter-{native_name}/src"
+    dotnet_dir = f"TreeSitter.{cs_name}"
+    run([
+        "gcc",
+        "-fPIC",
+        "-shared",
+        "-o", f"{dotnet_dir}/tree-sitter-{native_name}.{DLL}",
+        *[f"{native_dir}/{file}" for file in files],
+        f"-I{native_name}"
+    ], check=True)
+
+    print("    -- generating support code")
+    generate(f"{native_dir}/node-types.json", f"{dotnet_dir}/Generated.cs", cs_name)
+
+
+def build_managed():
+    print(" -- building dotnet libraries")
+    run(["dotnet", "build"], check=True)
+
+
+def main():
+    build_main_lib()
+    build_lang("c-sharp", "CSharp", "parser.c", "scanner.c")
+    build_managed()
+
+
+if __name__ == '__main__':
+    main()
